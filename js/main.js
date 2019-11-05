7bb355fcfff7e05c79d3fb584364b131ca30203f
lovedrones <JuanTron@Juans-MacBook-Pro.local>
2019-11-05 10:09:11 -0600

    js functions working-finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-// var myNewElement = $( "<li> </li>" );
+// remove list items for clean slate
+$('li').remove();
  
-// myNewElement.appendTo( "#devSkills" );
- 
+//varibles 
+ var $addList = $('#skills');
+var $rmv = ('#rmv');
+var $li = $('li');
+
+// functions-- to add to list 
+$('.addSkill').on('click', function() {
+    $addList.addClass('devList');
+    $('ul').append(`<li class='list'>
+                <button id='rmv'>${'X'}</button>${$addList.val()}</li>`)
+});
+//function to remove from lists
+$(document).on('click', '#devSkills', function(event){
+    $(event.target).closest('li').remove();
+  
+ });
+
 // myNewElement.insertAfter( "ul:last" ); // This will remove the p from #content!
  
 // $( "ul" ).last().after( myNewElement.clone() ); // Clone the p so now we have two.
