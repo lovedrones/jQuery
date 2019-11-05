de8829b41aef4d7408642b705a8e7b03f4451285
lovedrones <JuanTron@juans-mbp.attlocal.net>
2019-11-05 02:30:10 -0600

    dropmenu added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var myNewElement = $( "<li> </li>" );
+ 
+// myNewElement.appendTo( "#devSkills" );
+ 
+// myNewElement.insertAfter( "ul:last" ); // This will remove the p from #content!
+ 
+// $( "ul" ).last().after( myNewElement.clone() ); // Clone the p so now we have two.
